f2f23344cdf8d0b54797849e7257094a14d9fe08
Roman Matrosov <r.a.matrosov@gmail.com>
2021-11-19 09:00:07 +0200

    init

---
+++ app.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const { errors } = require('celebrate');
+const cors = require('cors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const error = require('./middlewares/error');
+const { MONGO_URL } = require('./config');
+const router = require('./routes');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+const CORS_WHITELIST = [
+  'http://localhost:3001',
+  'https://localhost:3001',
+  'https://movies.list.nomoredomains.rocks',
+  'http://movies.list.nomoredomains.rocks',
+];
+
+const corsOption = {
+  credentials: true,
+  origin: function checkCorsList(origin, callback) {
+    if (CORS_WHITELIST.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOption));
+
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(bodyParser.json());
+
+app.use(helmet());
+
+app.disable('x-powered-by');
+
+mongoose.connect(MONGO_URL);
+
+app.use(helmet());
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+app.use(router);
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use(error);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
